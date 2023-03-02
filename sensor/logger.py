72649e01a4e6adc2e03280782cc76029ab6092d8
agamdeepsing <agamdeep8467@gmail.com>
2023-03-02 22:22:07 +0530

    Data Ingestion Completed

--- sensor/logger.py
+++ sensor/logger.py
@@ -3,7 +3,7 @@ import os
 from datetime import datetime
 import os
 
-LOG_FILE_NAME = f"{datetime.now().strftime('%m%d%y_%H_%M_%S')}.log"
+LOG_FILE_NAME = f"{datetime.now().strftime('%m%d%y__%H_%M_%S')}.log"
 
 LOG_FILE_DIR = os.path.join(os.getcwd(),"logs")
 
@@ -12,5 +12,5 @@ LOG_FILE_PATH = os.path.join(LOG_FILE_DIR,LOG_FILE_NAME)
 logging.basicConfig(
     filename = LOG_FILE_PATH,
     format="[%(asctime)s ] %(lineno)d %(name)s -%(levelname)s -  %(message)s" ,
-    level=logging.INFO
+    level=logging.INFO,
 )
