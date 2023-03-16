a6caecdbe42186d8aaf140354b0e66776ceffc40
agamdeepsing <agamdeep8467@gmail.com>
2023-03-16 16:32:13 +0530

    data transformation completed.

--- sensor/entity/config_entity.py
+++ sensor/entity/config_entity.py
@@ -3,11 +3,13 @@ from sensor.exception import SensorException
 from sensor.logger import logging
 from datetime import datetime
 
-
 FILE_NAME = "sensor.csv"
 TRAIN_FILE_NAME = "train.csv"
 TEST_FILE_NAME = "test.csv"
 
+TRANSFORMER_OBJECT_FILE_NAME = "transformer.pkl"
+model_file_name = "model.pkl"
+TARGET_OBJECT_FILE_NAME = "target_encoder.pkl"
 
 class TrainingPipelineConfig:
 
@@ -49,12 +51,15 @@ class DataValidationConfig:
         seld.base_file_path = os.path.join("aps_failure_training_set1.csv")
 
 
+class DataTransformationConfig:
 
+    def __init__(self,training_pipeline_config:TrainingPipelineConfig):
+        self.data_transformation_dir = os.path.join(training_pipeline_config.artifact_dir , "data_transformation")
+        self.transform_object_path = os.path.join(self.data_transformation_dir,"transformer",TRANSFORMER_OBJECT_FILE_NAME)
+        self.transformed_train_path =  os.path.join(self.data_transformation_dir,"transformed",TRAIN_FILE_NAME)
+        self.transformed_test_path =os.path.join(self.data_transformation_dir,"transformed",TEST_FILE_NAME)
+        self.target_encoder_path = os.path.join(self.data_transformation_dir,"target_encoder",TARGET_ENCODER_OBJECT_FILE_NAME)
 
-
-
-
-class DataTransformationConfig:...
 class ModelTrainerConfig:...
 class ModelEvaluationConfig:...
 class ModelPusherConfig:...
